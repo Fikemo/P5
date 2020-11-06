bdc758eaf21ad0e99172fac33e541adf0e33693c
Fikemo <finnmorrison29@gmail.com>
2020-11-05 17:49:02 -0800

    Works. Might need some adjustments
    No work toward extra credit.
    Current goal is to build an iron axe.
    Takes 21.5 seconds

--- src/craft_planner.py
+++ src/craft_planner.py
@@ -1,6 +1,8 @@
 import json
 from collections import namedtuple, defaultdict, OrderedDict
 from timeit import default_timer as time
+from math import inf
+from heapq import heappop, heappush
 
 Recipe = namedtuple('Recipe', ['name', 'check', 'effect', 'cost'])
 
@@ -38,9 +40,21 @@ def make_checker(rule):
     # rule's requirements. This code runs once, when the rules are constructed before
     # the search is attempted.
 
+    # print(rule)
+
     def check(state):
         # This code is called by graph(state) and runs millions of times.
         # Tip: Do something with rule['Consumes'] and rule['Requires'].
+
+        if 'Consumes' in rule:
+            for item, value in rule['Consumes'].items():
+                if state[item] < value:  # don't have enough of that item
+                    return False
+
+        if 'Requires' in rule:
+            for item, value in rule['Requires'].items():
+                if state[item] == 0:
+                    return False
         return True
 
     return check
@@ -54,7 +68,16 @@ def make_effector(rule):
     def effect(state):
         # This code is called by graph(state) and runs millions of times
         # Tip: Do something with rule['Produces'] and rule['Consumes'].
-        next_state = None
+
+        next_state = State.copy(state)
+
+        for item, value in rule['Produces'].items():
+            next_state[item] += value  # add item quantity to inventory
+
+        if 'Consumes' in rule:
+            for item, value in rule['Consumes'].items():
+                next_state[item] -= value  # subtract item quantity from inventory
+
         return next_state
 
     return effect
@@ -66,7 +89,12 @@ def make_goal_checker(goal):
 
     def is_goal(state):
         # This code is used in the search process and may be called millions of times.
-        return False
+
+        for item, value in goal.items():
+            if state[item] < value:
+                return False
+
+        return True
 
     return is_goal
 
@@ -82,6 +110,30 @@ def graph(state):
 
 def heuristic(state):
     # Implement your heuristic here!
+
+    # From hint: If we already have a tool, we don't need to make another one.
+    # Or if we have plenty of a material, we don't need any more.
+    # This works as long as the goal isn't to build more than 1 tool or to acquire more material than specified by the heuristic
+
+    tools = ['furnace', 'bench', 'wooden_pickaxe', 'wooden_axe', 'stone_pickaxe', 'stone_axe', 'iron_pickaxe', 'iron_axe']
+    materials_with_heuristic_value = {
+        'coal':   1,
+        'cobble': 8,
+        'wood':   1,
+        'plank':  8,
+        'stick':  4,
+        'ore':    1,
+        'ingot':  6,
+    }
+
+    for tool in tools:
+        if state[tool] > 1:
+            return inf
+
+    for material in materials_with_heuristic_value.keys():
+        if state[material] > materials_with_heuristic_value[material]:
+            return inf
+
     return 0
 
 def search(graph, state, is_goal, limit, heuristic):
@@ -92,13 +144,48 @@ def search(graph, state, is_goal, limit, heuristic):
     # When you find a path to the goal return a list of tuples [(state, action)]
     # representing the path. Each element (tuple) of the list represents a state
     # in the path and the action that took you to this state
+
+    queue = [(0, state, None)]  # This is like the frontier from other assignments, but I don't think it's a frontier if the nodes don't represent a physical space
+
+    cost = {state: 0}
+
+    prev = {state: None}
+
+    actions = {state: None}
+
+    visited = [state]
+
+    path = []
+
     while time() - start_time < limit:
-        pass
+
+        current_cost, current_state, current_action = heappop(queue)
+
+        if is_goal(current_state):  # found the goal, now construct the path
+            path.append((current_state, current_action))
+            current_back_node = prev[current_state]
+            while current_back_node is not None:
+                path.insert(0, (current_back_node, actions[current_back_node]))
+                current_back_node = prev[current_back_node]
+            break
+
+        for node_action, node_state, node_cost in graph(current_state):
+            new_cost = current_cost + node_cost
+            if(node_state not in cost or new_cost < cost[node_state]) and node_state not in visited:
+                cost[node_state] = new_cost
+                actions[node_state] = node_action
+                visited.append(node_state)
+                priority = new_cost + heuristic(node_state)
+                heappush(queue, (priority, node_state, node_action))
+                prev[node_state] = current_state
 
     # Failed to find a path
     print(time() - start_time, 'seconds.')
+    if path:
+        return path
     print("Failed to find a path from", state, 'within time limit.')
     return None
+
 
 if __name__ == '__main__':
     with open('Crafting.json') as f:
@@ -132,10 +219,10 @@ if __name__ == '__main__':
     state.update(Crafting['Initial'])
 
     # Search for a solution
-    resulting_plan = search(graph, state, is_goal, 5, heuristic)
+    resulting_plan = search(graph, state, is_goal, 30, heuristic)
 
     if resulting_plan:
         # Print resulting plan
         for state, action in resulting_plan:
-            print('\t',state)
+            print('\t', state)
             print(action)
